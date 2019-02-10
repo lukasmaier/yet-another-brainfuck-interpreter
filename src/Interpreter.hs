87be9040e51180cf0583474adad27c251c6b29bf
Lukas Maier <lukasmaier21@hotmail.de>
2019-02-10 23:39:50 +0100

    cleaned up the code a little bit

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -5,25 +5,25 @@ import qualified Data.Sequence as S
 data Tape = TP (S.Seq Int) Int
 
 interpret :: String -> IO Tape
-interpret code = interpret' code (TP (S.singleton 0) 0)
+interpret code = interpret' code $ TP (S.singleton 0) 0
 
 interpret' :: String -> Tape -> IO Tape
 interpret' code@(c:cs) tape@(TP seq i) = case c of 
                                     '>' ->  if i == (S.length seq - 1) then
-                                                interpret' cs (TP (seq S.|> 0) (i + 1))
+                                                interpret' cs $ TP (seq S.|> 0) (i + 1)
                                             else
-                                                interpret' cs (TP seq (i + 1))
+                                                interpret' cs $ TP seq (i + 1)
                                     '<' ->  if i == 0 then
-                                                interpret' cs (TP (0 S.<| seq) i)
+                                                interpret' cs $ TP (0 S.<| seq) i
                                             else 
-                                                interpret' cs (TP seq (i-1))
-                                    '+' -> interpret' cs (TP (S.update i (S.index seq i + 1) seq) i)
-                                    '-' -> interpret' cs (TP (S.update i (S.index seq i - 1) seq) i)
+                                                interpret' cs $ TP seq (i-1)
+                                    '+' -> interpret' cs $ TP (S.update i (S.index seq i + 1) seq) i
+                                    '-' -> interpret' cs $ TP (S.update i (S.index seq i - 1) seq) i
                                     '[' -> loop 0 [] cs tape
                                     ']' -> error "'[' needed before ']'"
                                     '.' -> putChar (toEnum (S.index seq i) :: Char) >> interpret' cs (TP seq i)
-                                    ',' -> getChar >>= \q -> interpret' cs (TP (S.update i (fromEnum q) seq) i)
-                                    _   -> interpret' cs (TP seq i)
+                                    ',' -> getChar >>= \q -> interpret' cs $ TP (S.update i (fromEnum q) seq) i
+                                    _   -> interpret' cs $ TP seq i
 interpret' _ tape = return tape
 
 
